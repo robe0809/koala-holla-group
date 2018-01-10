c5fac1745f22f7edb3f655cbf935900f9890e286
Amy Richardson <amyrichardson96@gmail.com>
2018-01-10 14:05:27 -0600

    finished delete request

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,6 +5,8 @@ $(document).ready(onReady);
 function onReady() {
   console.log('jq');
   getKoalas();
+  $('#addButton').on('click', addKoala);
+  $('#viewKoalas').on('click', '.terminateKoala', terminateKoala);
 }
 
 function getKoalas(){
@@ -26,10 +28,15 @@ function appendKoalas (koalasArr) {
     $rows.append('<td>' + koalasArr[i].age + '</td>');
     $rows.append('<td>' + koalasArr[i].ready_to_transfer + '</td>');
     $rows.append('<td>' + koalasArr[i].notes + '</td>');
+    if(koalasArr[i].ready_to_transfer == 'N'){
+      $rows.append('<td><button class="transfer">Transfer Koala</button></td>');
+    } else {
+      $rows.append('<td></td>');
+    }
+    $rows.append('<td><button data-id="' + koalasArr[i].id + '" class="terminateKoala">Terminate Koala</button><td>');
     $('#viewKoalas').append($rows);
   }
 }
-
 class Koala{
   constructor(nameIn, ageIn, genderIn, readyForTransferIn, notesIn){
       this.name = nameIn;
@@ -40,9 +47,36 @@ class Koala{
   }//end constructor
 }//end Koala Class
 
-
 function addKoala(){
   let name = $('#nameIn').val();
-  let koala = new Koala(  )
+  let age = $('#ageIn').val();
+  let gender = $('#genderIn').val();
+  let ready_to_transfer = $('#readyForTransferIn').val();
+  if( $('#readyForTransferIn').is(':checked') ){
+    ready_to_transfer = 'Y';
+  }
+  
+  let notes = $('#notesIn').val();
+  let koala = new Koala(name, age, gender, ready_to_transfer, notes);
 
+  $.ajax({
+    method: 'POST',
+    url: '/koalas',
+    data: koala,
+    success: function(response){
+      $('#viewKoalas').empty();
+      getKoalas();
+    }
+  });
+}//end addKoala
+function terminateKoala() {
+  let id = $(this).data('id');
+  $(this).parent().parent().remove();
+  $.ajax({
+    method: "DELETE",
+    url: '/koalas/'+ id,
+    success: function (response) {
+      console.log('successful response', response);
+    }
+  })
 }
--- server/routes/koalas.router.js
+++ server/routes/koalas.router.js
@@ -16,4 +16,33 @@ router.get('/', (req, res) => {
         })
 });
 
+router.post('/', (req,res) => {
+   
+    const queryText = 'INSERT INTO koala(name, gender, age, ready_to_transfer, notes)VALUES($1, $2, $3, $4, $5)';
+    pool.query(queryText, [req.body.name, req.body.gender, req.body.age, req.body.ready_to_transfer, req.body.notes])
+        .then((result) => {
+            console.log('successfully posted');
+            
+            res.sendStatus(201);
+        })
+        .catch((err) => {
+            console.log('error:', err);
+            res.sendStatus(500);
+        })
+
+});
+
+router.delete('/:id', (req, res) => {
+    const queryText = 'DELETE FROM koala WHERE id= $1';
+    pool.query(queryText, [req.params.id])
+        .then((result) => {
+            console.log('results: ', req.params.id);
+            res.sendStatus(200);
+        })
+        .catch((err) => {
+            console.log('error:', err);
+            res.sendStatus(500);
+        })
+})
+
 module.exports = router;
